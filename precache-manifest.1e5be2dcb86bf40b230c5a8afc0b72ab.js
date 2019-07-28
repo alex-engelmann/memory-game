b97ce1c59ac46366063a850bc54497891b09b8ce
Alex Engelmann <alex.engelmann@gmail.com>
2019-07-27 19:22:50 -0700

    Updates

---
+++ precache-manifest.1e5be2dcb86bf40b230c5a8afc0b72ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d6fec265ea61977ac3f49b70d33486",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "c04f3b67e64ce7262c4f",
+    "url": "/memory-game/static/css/main.7df1af28.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/memory-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c04f3b67e64ce7262c4f",
+    "url": "/memory-game/static/js/main.892c514e.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
